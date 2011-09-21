6715f0450208d2efd28b1818fa4884db31622347
Emil Hernvall <emil@quench.at>
2011-09-21 22:07:16 +0200

    Made an attempt at implementing gouraud shading. It's buggy as fuck.

--- src/game/GraphicsEngine.java
+++ src/game/GraphicsEngine.java
@@ -14,12 +14,14 @@ public class GraphicsEngine
     private int width, height;
     private Vector3D camera, observer;
     private List<LightSource> lightsources;
-    private List<Vertex> vertices;
+    private List<Polygon> polygons;
+    private Map<Vector3D, List<Polygon>> vertices;
     private float[] zBuffer;
     
     private float rotX = 0.0f;
     private float rotZ = 0.0f;
     
+    private double ambientIntensity = 1.0;
     private int focalLength = 500;
 
     public GraphicsEngine(int width, int height)
@@ -30,7 +32,8 @@ public class GraphicsEngine
         this.zBuffer = new float[width*height];
         
         this.lightsources = new ArrayList<LightSource>();
-        this.vertices = new ArrayList<Vertex>();
+        this.polygons = new ArrayList<Polygon>();
+        this.vertices = new HashMap<Vector3D, List<Polygon>>();
         
         this.observer = new Vector3D(0, 0, -5);
     }
@@ -90,39 +93,52 @@ public class GraphicsEngine
         lightsources.add(l);
     }
     
-    public void addVertex(Vertex v)
+    public void addPolygon(Polygon p)
     {
-        vertices.add(v);
+        polygons.add(p);
+        addVertice(p.a, p);
+        addVertice(p.b, p);
+        addVertice(p.c, p);
+    }
+    
+    private void addVertice(Vector3D edge, Polygon p)
+    {
+        List<Polygon> edges = vertices.get(edge);
+        if (edges == null) {
+            edges = new ArrayList<Polygon>();
+            vertices.put(edge, edges);
+        }
+        edges.add(p);
     }
     
     public void addCube(Vector3D a, Vector3D b, Color color)
     {
         // surfaces parallell to the xy plane
         // (a.x, a.y, a.z) - (b.x, b.y, a.z)
-        vertices.add(new Vertex(a.x, a.y, a.z,  b.x, a.y, a.z,  a.x, b.y, a.z, color));
-        vertices.add(new Vertex(b.x, b.y, a.z,  b.x, a.y, a.z,  a.x, b.y, a.z, color));
+        addPolygon(new Polygon(a.x, a.y, a.z,  b.x, a.y, a.z,  a.x, b.y, a.z, color));
+        addPolygon(new Polygon(b.x, b.y, a.z,  b.x, a.y, a.z,  a.x, b.y, a.z, color));
         
         // (a.x, a.y, b.z) - (b.x, b.y, b.z)
-        vertices.add(new Vertex(a.x, a.y, b.z,  b.x, a.y, b.z,  a.x, b.y, b.z, color));
-        vertices.add(new Vertex(b.x, b.y, b.z,  b.x, a.y, b.z,  a.x, b.y, b.z, color));
+        addPolygon(new Polygon(a.x, a.y, b.z,  b.x, a.y, b.z,  a.x, b.y, b.z, color));
+        addPolygon(new Polygon(b.x, b.y, b.z,  b.x, a.y, b.z,  a.x, b.y, b.z, color));
         
         // surfaces parallell to xz plane
         // (a.x, a.y, a.z) - (b.x, a.y, b.z)
-        vertices.add(new Vertex(a.x, a.y, a.z,  a.x, a.y, b.z,  b.x, a.y, a.z, color));
-        vertices.add(new Vertex(b.x, a.y, b.z,  a.x, a.y, b.z,  b.x, a.y, a.z, color));
+        addPolygon(new Polygon(a.x, a.y, a.z,  a.x, a.y, b.z,  b.x, a.y, a.z, color));
+        addPolygon(new Polygon(b.x, a.y, b.z,  a.x, a.y, b.z,  b.x, a.y, a.z, color));
         
         // (a.x, b.y, a.z) - (b.x, b.y, b.z)
-        vertices.add(new Vertex(a.x, b.y, a.z,  a.x, b.y, b.z,  b.x, b.y, a.z, color));
-        vertices.add(new Vertex(b.x, b.y, b.z,  a.x, b.y, b.z,  b.x, b.y, a.z, color));
+        addPolygon(new Polygon(a.x, b.y, a.z,  a.x, b.y, b.z,  b.x, b.y, a.z, color));
+        addPolygon(new Polygon(b.x, b.y, b.z,  a.x, b.y, b.z,  b.x, b.y, a.z, color));
         
         // surfaces parallell to yz plane
         // (a.x, a.y, a.z) - (a.x, b.x, b.z)
-        vertices.add(new Vertex(a.x, a.y, a.z,  a.x, a.y, b.z,  a.x, b.y, a.z, color));
-        vertices.add(new Vertex(a.x, b.y, b.z,  a.x, a.y, b.z,  a.x, b.y, a.z, color));
+        addPolygon(new Polygon(a.x, a.y, a.z,  a.x, a.y, b.z,  a.x, b.y, a.z, color));
+        addPolygon(new Polygon(a.x, b.y, b.z,  a.x, a.y, b.z,  a.x, b.y, a.z, color));
         
         // (b.x, a.y, a.z) - (b.x, b.x, b.z)
-        vertices.add(new Vertex(b.x, a.y, a.z,  b.x, a.y, b.z,  b.x, b.y, a.z, color));
-        vertices.add(new Vertex(b.x, b.y, b.z,  b.x, a.y, b.z,  b.x, b.y, a.z, color));
+        addPolygon(new Polygon(b.x, a.y, a.z,  b.x, a.y, b.z,  b.x, b.y, a.z, color));
+        addPolygon(new Polygon(b.x, b.y, b.z,  b.x, a.y, b.z,  b.x, b.y, a.z, color));
     }
     
     public void addSphere(Vector3D center, double radius, int steps, Color color)
@@ -150,8 +166,8 @@ public class GraphicsEngine
                 
                 double z2 = center.z + radius*Math.cos(theta2);
                 
-                vertices.add(new Vertex(x11, y11, z1,  x12, y12, z1,  x21, y21, z2, color));
-                vertices.add(new Vertex(x22, y22, z2,  x12, y12, z1,  x21, y21, z2, color));
+                addPolygon(new Polygon(x11, y11, z1,  x12, y12, z1,  x21, y21, z2,  color));
+                addPolygon(new Polygon(x22, y22, z2,  x12, y12, z1,  x21, y21, z2,  color));
             }
         }
     }
@@ -168,32 +184,51 @@ public class GraphicsEngine
         }
     }
     
+    private double calcIntensity(Vector3D v)
+    {
+        // average the normals of all edges that intersect at
+        // this vertice
+        List<Polygon> polygons = vertices.get(v);
+        Vector3D normal = null;
+        for (Polygon p : polygons) {
+            if (normal == null) {
+                normal = p.normal();
+            } else {
+                normal = normal.add(p.normal());
+            }
+        }
+        normal = normal.mul(1.0/polygons.size());
+    
+        double intensity = ambientIntensity;
+        for (LightSource l : lightsources) {
+            Vector3D rel = v.sub(l.position);
+            
+            double i = normal.dot(rel);
+            if (!Double.isNaN(i)) {
+                intensity += i * l.intensity;
+            }
+        }
+        
+        return Math.abs(intensity);
+    }
+    
     public void drawScene()
     {
         long time = System.currentTimeMillis();
         
         resetZBuffer();
         
-        List<Vertex> renderList = new ArrayList<Vertex>();
+        List<Polygon> renderList = new ArrayList<Polygon>();
             
         double intensityNorm = 0.0;
-        for (Vertex v : vertices) {
+        for (Polygon v : polygons) {
             // calculate color
-            Vector3D first = v.b.sub(v.a);
-            Vector3D second = v.c.sub(v.a);
-            
-            Vector3D normal = first.cross(second).norm();
-            double intensity = 0.0;
-            for (LightSource l : lightsources) {
-                Vector3D rel = v.a.sub(l.position);
-                
-                double i = normal.dot(rel) * l.intensity;
-                if (!Double.isNaN(i)) {
-                    intensity += i;
-                }
-            }
-            
-            intensityNorm = Math.max(intensityNorm, intensity);
+            double aIntensity = calcIntensity(v.a);
+            intensityNorm = Math.max(intensityNorm, aIntensity);
+            double bIntensity = calcIntensity(v.b);
+            intensityNorm = Math.max(intensityNorm, bIntensity);
+            double cIntensity = calcIntensity(v.c);
+            intensityNorm = Math.max(intensityNorm, cIntensity);
             
             // change coordinates to camera position
             Vector3D a = v.a.sub(camera);
@@ -205,30 +240,33 @@ public class GraphicsEngine
             b = b.rotZ(rotZ).rotX(rotX);
             c = c.rotZ(rotZ).rotX(rotX);
             
-            renderList.add(new Vertex(a, b, c, v.color, intensity));
+            Polygon p = new Polygon(a, b, c, v.color);
+            p.aIntensity = aIntensity;
+            p.bIntensity = bIntensity;
+            p.cIntensity = cIntensity;
+            renderList.add(p);
         }
         
         // normalize colors and draw
         int drawn = 0;
-        for (Vertex v : renderList) {
-            v.intensity /= intensityNorm;
-            v.color = new Color(
-                    (int)Math.abs(v.color.getRed() * v.intensity),
-                    (int)Math.abs(v.color.getGreen() * v.intensity),
-                    (int)Math.abs(v.color.getBlue() * v.intensity)
-                );
+        for (Polygon v : renderList) {
+            v.aIntensity /= intensityNorm;
+            v.bIntensity /= intensityNorm;
+            v.cIntensity /= intensityNorm;
+            
+            //System.out.println(v.aIntensity + ", " + v.bIntensity + ", " + v.cIntensity);
                 
-            if (drawVertex(v)) {
+            if (drawPolygon(v)) {
                 drawn++;
             }
         }
         
         time = System.currentTimeMillis() - time;
         
-        System.out.println("drew " + drawn + " vertices in " + time + " ms.");
+        //System.out.println("drew " + drawn + " polygons in " + time + " ms.");
     }
     
-    private boolean drawVertex(Vertex v)
+    private boolean drawPolygon(Polygon v)
     {
         Vector3D a = v.a;
         Vector3D b = v.b;
@@ -243,14 +281,17 @@ public class GraphicsEngine
         int x1 = (int)(a.x * focalLength/a.y) + width / 2;
         int y1 = (int)(a.z * focalLength/a.y) + height / 2;
         int z1 = (int)a.y;
+        double i1 = v.aIntensity;
         
         int x2 = (int)(b.x * focalLength/b.y) + width / 2;
         int y2 = (int)(b.z * focalLength/b.y) + height / 2;
         int z2 = (int)b.y;
+        double i2 = v.bIntensity;
         
         int x3 = (int)(c.x * focalLength/c.y) + width / 2;
         int y3 = (int)(c.z * focalLength/c.y) + height / 2;
         int z3 = (int)c.y;
+        double i3 = v.cIntensity;
         
         if (
                 ((x1 < 0 || x1 > width) && (y1 < 0 || y1 > height)) ||
@@ -263,20 +304,24 @@ public class GraphicsEngine
         
         // draw triangle
         int tmp;
+        double tmp2;
         if (y1 > y2) { 
             tmp = x1; x1 = x2; x2 = tmp;
             tmp = y1; y1 = y2; y2 = tmp;
             tmp = z1; z1 = z2; z2 = tmp;
+            tmp2 = i1; i1 = i2; i2 = tmp2;
         }
         if (y1 > y3) { 
             tmp = x1; x1 = x3; x3 = tmp;
             tmp = y1; y1 = y3; y3 = tmp;
             tmp = z1; z1 = z3; z3 = tmp;
+            tmp2 = i1; i1 = i3; i3 = tmp2;
         }
         if (y2 > y3) {
             tmp = x2; x2 = x3; x3 = tmp;
             tmp = y2; y2 = y3; y3 = tmp;
             tmp = z2; z2 = z3; z3 = tmp;
+            tmp2 = i2; i2 = i3; i3 = tmp2;
         }
         
         double[] x12 = MathUtils.linearInterpolation(y1, x1, y2, x2);
@@ -286,19 +331,31 @@ public class GraphicsEngine
         double[] z12 = MathUtils.linearInterpolation(y1, z1, y2, z2);
         double[] z23 = MathUtils.linearInterpolation(y2, z2, y3, z3);
         double[] z13 = MathUtils.linearInterpolation(y1, z1, y3, z3);
+        
+        double[] i12 = MathUtils.linearInterpolation(y1, i1, y2, i2);
+        double[] i23 = MathUtils.linearInterpolation(y2, i2, y3, i3);
+        double[] i13 = MathUtils.linearInterpolation(y1, i1, y3, i3);
 
         for (int y = y1; y < y2; y++) {
-            drawSegment((int)x12[y-y1], y, (int)z12[y-y1], (int)x13[y-y1], y, (int)z13[y-y1], v.color);
+            drawSegment(
+                    (int)x12[y-y1], y, (int)z12[y-y1], i12[y-y1],
+                    (int)x13[y-y1], y, (int)z13[y-y1], i13[y-y1],
+                    v.color
+                );
         }
 
         for (int y = y2; y < y3; y++) {
-            drawSegment((int)x23[y-y2], y, (int)z23[y-y2], (int)x13[y-y1], y, (int)z13[y-y1], v.color);
+            drawSegment(
+                    (int)x23[y-y2], y, (int)z23[y-y2], i23[y-y2],
+                    (int)x13[y-y1], y, (int)z13[y-y1], i13[y-y1],
+                    v.color
+                );
         }
         
         return true;
     }
     
-    private void drawSegment(int x0, int y0, int z0, int x1, int y1, int z1, Color color)
+    private void drawSegment(int x0, int y0, int z0, double i0, int x1, int y1, int z1, double i1, Color color)
     {
         int dx = Math.abs(x1 - x0);
         int dy = Math.abs(y1 - y0);
@@ -312,11 +369,12 @@ public class GraphicsEngine
 
             double[] y_values = MathUtils.linearInterpolation(x0, y0, x1, y1);
             double[] z_values = MathUtils.linearInterpolation(x0, z0, x1, z1);
+            double[] i_values = MathUtils.linearInterpolation(x0, i0, x1, i1);
             for (int x = x0; x < x1; x++) {
-                putPixel(x, (int)y_values[x-x0], (float)z_values[x-x0], color);
+                putPixel(x, (int)y_values[x-x0], (float)z_values[x-x0], color, i_values[x-x0]);
             }
         }
-        else {
+        /*else {
             if (y0 > y1) {
                 tmp = x0; x0 = x1; x1 = tmp;
                 tmp = y0; y0 = y1; y1 = tmp;
@@ -324,13 +382,14 @@ public class GraphicsEngine
 
             double[] x_values = MathUtils.linearInterpolation(y0, x0, y1, x1);
             double[] z_values = MathUtils.linearInterpolation(y0, z0, y1, z1);
+            double[] i_values = MathUtils.linearInterpolation(y0, i0, y1, i1);
             for (int y = y0; y < y1; y++) {
-                putPixel((int)x_values[y-y0], y, (float)z_values[y-y0], color);
+                putPixel((int)x_values[y-y0], y, (float)z_values[y-y0], color, i_values[y-y0]);
             }
-        }
+        }*/
     }
     
-    private void putPixel(int x, int y, float z, Color color)
+    private void putPixel(int x, int y, float z, Color color, double intensity)
     {
         if (x < 0 || x >= width) {
             return;
@@ -340,7 +399,12 @@ public class GraphicsEngine
         }
     
         if (z < zBuffer[width*y + x]) {
-            surface.putPixel(x, y, color);
+            Color newColor = new Color(
+                    (int)(color.getRed() * intensity),
+                    (int)(color.getGreen() * intensity),
+                    (int)(color.getBlue() * intensity)
+                );
+            surface.putPixel(x, y, newColor);
             zBuffer[width*y + x] = z;
         }
     }
--- src/game/Polygon.java
+++ src/game/Polygon.java
@@ -2,28 +2,32 @@ package game;
 
 import java.awt.*;
 
-public class Vertex
+public class Polygon
 {
     public Vector3D a, b, c;
     public Color color;
-    public double intensity;
+    public double aIntensity, bIntensity, cIntensity;
     
-    public Vertex(Vector3D a, Vector3D b, Vector3D c, Color color, double intensity)
+    public Polygon(Vector3D a, Vector3D b, Vector3D c, Color color)
     {
         this.a = a;
         this.b = b;
         this.c = c;
         this.color = color;
-        this.intensity = intensity;
+        this.aIntensity = 0.0;
+        this.bIntensity = 0.0;
+        this.cIntensity = 0.0;        
     }
     
-    public Vertex(double x1, double y1, double z1, double x2, double y2, double z2, double x3, double y3, double z3, Color color)
+    public Polygon(double x1, double y1, double z1, double x2, double y2, double z2, double x3, double y3, double z3, Color color)
     {
         this.a = new Vector3D(x1, y1, z1);
         this.b = new Vector3D(x2, y2, z2);
         this.c = new Vector3D(x3, y3, z3);
         this.color = color;
-        this.intensity = 0.0;
+        this.aIntensity = 0.0;
+        this.bIntensity = 0.0;
+        this.cIntensity = 0.0;
     }
     
     public double centroidY()
@@ -38,6 +42,14 @@ public class Vertex
                 (a.y + b.y + c.y) / 3.0,
                 (a.z + b.z + c.z) / 3.0
             );
+    }
+    
+    public Vector3D normal()
+    {
+        Vector3D first = b.sub(a);
+        Vector3D second = c.sub(a);
+        
+        return first.cross(second).norm();
     }
     
     @Override
